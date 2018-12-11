841afb486c47bbd4174a6af6b8fa9cd7308cf16d
Seung Jae Hyun <seungjaehyun@Seungs-MacBook.local>
2018-12-10 21:51:45 -0800

    added https

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ var pokemonStats = document.getElementsByClassName("pokemon-stats")[0];
 var loading = document.getElementsByClassName("loading-animation")[0];
 
 function apiurl(pokemon){
-    return `http://pokeapi.salestock.net/api/v2/pokemon/${pokemon}`;
+    return `https://pokeapi.salestock.net/api/v2/pokemon/${pokemon}`;
 }
 
 function submitPokemon(){
